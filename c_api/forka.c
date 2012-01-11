9df30ebbf153ebeb8834b9c74d7942dc1a1690c3
tongchuhuozhai <feasible123@163.com>
2012-01-11 11:36:07 +0800

    Add file open learn.
    Signed-off-by: tongchuhuozhai <feasible123@163.com>

--- c_api/forka.c
+++ c_api/forka.c
@@ -2,9 +2,10 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
-#include </home/liyunpeng/learn/c_api/threada.h>
+#include </home/tongchuhuozhai/learn1/c_api/threada.h>
 #include <stdarg.h>
 #include <string.h>
+#include <fcntl.h>
 
 void var_multi(const char* p, ...)
 {
@@ -13,7 +14,10 @@ void var_multi(const char* p, ...)
 	// last fixed para
 	printf("%s\n", p);
 
+	// declare pointer
 	va_list p_list;
+
+	// define pointer
 	va_start(p_list, p);
 
 	// second variable para
@@ -26,7 +30,21 @@ void var_multi(const char* p, ...)
 
 
 	va_end(p_list);
-		
+}
+
+void file_t()
+{
+	int fd = open("/tmp/file_test_a", O_WRONLY | O_CREAT);
+	char a[] = "data";
+	write(fd, a, sizeof(a));
+	close(fd);
+
+	char buffer[10];
+	fd = open("/tmp/file_test_a", O_RDONLY);
+	read(fd, buffer, 3);
+	close(fd);
+	printf("%s\n", buffer);
+
 }
 
 int main()
@@ -34,9 +52,14 @@ int main()
 	pid_t pid = fork();
 	if( pid == 0 ) {
 		printf("sub process beging... \n ");
+		// define the vector of command parameters
 		char *v[] = {"ls", "-l", NULL};
+
+		// define enviroment varialbles 
 		char *e[] = {"PATH=/bin/ls", NULL};
+
 		execve("/bin/ls", v, e);
+
 		// why it doesn't print
 		printf("sub process end. " );
 
@@ -45,9 +68,7 @@ int main()
 		waitpid(pid, NULL, 0); 
 		printf("parent process receive msg that sub process exit. \n" ); 
 		start_threada();
-		var_multi("para1", "para2");
+		var_multi("para1", "para2", "para3");
+		file_t();
    	}
 }
-
-
-	
