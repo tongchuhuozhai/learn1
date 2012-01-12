7766e49d572a3204e80580741b243b4d7854b7e6
gang.li <gang.li@eavoo.com>
2012-01-12 18:35:55 +0800

    day end commit.
    Signed-off-by: gang.li <gang.li@eavoo.com>

--- hellomod/hellomod.c
+++ hellomod/hellomod.c
@@ -1,6 +1,14 @@
 #include <linux/module.h>
 #include <linux/init.h>
 MODULE_LICENSE("GPL");
+// 0x12345678 could be a hardware address, define a poninter 
+// which points this address, this pointer's type is volatile 
+// unsigned. 
+#define hw_pointer ((volatile unsigned*)0x12345678)
+
+// define the variable to get the content of hardware address
+#define hw_pointer_value (*hw_pointer)
+
 static int __init hello_init(void)
 {
 	printk("init ");
