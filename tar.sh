36cae2828378c70b8f324116bc32a8b1f909b9e7
lg <ligang@lg-desktop.(none)>
2013-04-11 14:54:54 +0800

    指定tar压缩后的文件目录为save-to-network
    Signed-off-by: lg <ligang@lg-desktop.(none)>

--- tar.sh
+++ tar.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-tar -czvf ../learn1.tar.gz ../learn1 --exclude=*.git --exclude=*.ko --exclude=a --exclude=*.cmd  --exclude=*.o --exclude=*.mod --exclude=*.symvers --exclude=*.swp
+tar -czvf ~/save-to-network/learn1.tar.gz ../learn1 --exclude=*.git --exclude=*.ko --exclude=a --exclude=*.cmd  --exclude=*.o --exclude=*.mod --exclude=*.symvers --exclude=*.swp
