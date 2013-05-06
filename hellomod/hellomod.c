7ff3d0ad36a83e7978ee47f7d0ea79bdc413c264
ligang <ligang@ligang-System-Product-Name.(none)>
2013-05-06 16:10:43 +0800

    test
    Signed-off-by: ligang <ligang@ligang-System-Product-Name.(none)>

--- hellomod/hellomod.c
+++ hellomod/hellomod.c
@@ -1,6 +1,6 @@
 #include <linux/interrupt.h>
 #include <linux/module.h>
-#include <asm/uaccess.h> // 如果不包含这个文件，则copy_to_user报兼容性错误
+#include <asm/uaccess.h> // if not contain this file, copy_to_user would error for compitiple error.
 #include <linux/init.h>
 #include <linux/cdev.h>
 #include <linux/sched.h>
@@ -24,6 +24,7 @@
 #include <linux/rwsem.h>
 #include <linux/nsproxy.h>
 #include <linux/ipc_namespace.h>
+#include "hellomod.h"
 /*
 #include <asm/io.h>
 #include <asm/system.h>
@@ -31,18 +32,18 @@
 */
 
 MODULE_LICENSE("GPL");
-// 0x12345678 could be a hardware address, define a poninter 
-// which points this address, this pointer's type is volatile 
-// unsigned. 
+/*
+0x12345678 could be a hardware address, define a poninter 
+ which points this address, this pointer's type is volatile 
+ unsigned. 
+ 
+ */
 #define hw_pointer ((volatile unsigned*)0x12345678)
 
 // define the variable to get the content of hardware address
 #define hw_pointer_value (*hw_pointer)
 
-struct globalmem_dev {
-	struct cdev cdev;	
-	char mem[100];
-};
+
 
 struct globalmem_dev *globalmem_devp;
 
@@ -91,6 +92,7 @@ static int __init hello_init(void)
 	}
 
 	globalmem_devp = kmalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+	memset(globalmem_devp, 0, sizeof(globalmem_devp);
 
 	// init char device
 	#if 1
@@ -131,6 +133,13 @@ void l_work_queue(){
 	INIT_DELAYED_WORK(dw, func);
 }
 
+void l_work_queue_1()
+{
+	wait_queue_head_t queue_head;
+	init_waitqueue_head(&queue_head);
+	//DECLARE_WAITQUEUE("queue1", );	
+	
+}
 void irq_handle()
 {
 	printk("irq_handle");
@@ -145,12 +154,19 @@ void l_irq()
 
 static void __exit hello_exit(void)
 {
+
 	cdev_del(&globalmem_devp->cdev);
 	kfree(globalmem_devp);
 	free_irq(irq_number, device_name);
 	printk("hello exit. " );
 }
 
+long sub_add1(int a, int b)
+{
+	return a+b;
+}
+
+EXPORT_SYMBOL(sub_add1);
 
 module_init(hello_init);
 module_exit(hello_exit);
