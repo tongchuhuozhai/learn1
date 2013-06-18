e91fec6a9bf9fbda3445d86059fa3e0534caa5c3
ligang <ligang@ligang-System-Product-Name.(none)>
2013-06-18 15:54:44 +0800

    use expr
    Signed-off-by: ligang <ligang@ligang-System-Product-Name.(none)>

--- shell/a.sh
+++ shell/a.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
-line=1
+line=3
+line1=6
+diff=`expr $line1 \- $line`
+echo $diff
 # read from 1 line to 5 line in a.txt
-sed -n "`echo $line`, 5p" a.txt
+sed -n "`echo $line`, 5p" a.txt >> f.txt
+sed -n "`echo $diff`, 5p" a.txt >> diff.txt
