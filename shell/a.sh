ad3deb1c06c82bc08e7bcc4fd90496cbdd1012f8
ligang <ligang@ligang-System-Product-Name.(none)>
2013-06-19 15:16:19 +0800

    the method to check if variable has already been defined
    Signed-off-by: ligang <ligang@ligang-System-Product-Name.(none)>

--- shell/a.sh
+++ shell/a.sh
@@ -1,8 +1,19 @@
 #! /bin/sh
 line=3
 line1=6
-diff=`expr $line1 \- $line`
-echo $diff
+linea=1
+
+#if linea variblea is defined, enter
+if [ -n "$linea" ]; then
+   echo "linea is defined"
+   diff=`expr $line1 \- $linea`
+else
+   diff=1
+   echo "linea is not defined" 
+fi
+
+echo "diff is:" $diff
 # read from 1 line to 5 line in a.txt
 sed -n "`echo $line`, 5p" a.txt >> f.txt
-sed -n "`echo $diff`, 5p" a.txt >> diff.txt
+sed -n "`echo $diff`, `echo $line1`p" a.txt
+tar hcjvf 1.tar.bz2 a.txt f.txt c.txt
